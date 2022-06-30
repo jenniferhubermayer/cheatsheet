277f11f1fe2c30d6153036cfe6fea04137d770ac
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-06-30 12:03:39 +0200

    Updated with JS and new section for JS-Methods

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+document.querySelector("#git-kommandos").classList.add("active")
+document.querySelector("#terminal-kommandos").classList.remove("active")
+
+showTopic = () =>{
+    let topic = document.querySelector("#topic").value
+    switch (topic){
+        case "showGit":
+            document.querySelector("#git-kommandos").classList.add("active")
+            document.querySelector("#terminal-kommandos").classList.remove("active")
+            document.querySelector("#terminal-kommandos").classList.add("inactive")
+            document.querySelector("#js-methoden").classList.remove("active")
+            document.querySelector("#js-methoden").classList.add("inactive")
+            break;
+    }
+    switch (topic){
+        case "showTerminal":
+            document.querySelector("#terminal-kommandos").classList.add("active")
+            document.querySelector("#git-kommandos").classList.remove("active")
+            document.querySelector("#git-kommandos").classList.add("inactive")
+            document.querySelector("#js-methoden").classList.remove("active")
+            document.querySelector("#js-methoden").classList.add("inactive")
+            break;
+    }
+    switch (topic){
+        case "showJSMethoden":
+            document.querySelector("#js-methoden").classList.add("active")
+            document.querySelector("#git-kommandos").classList.remove("active")
+            document.querySelector("#git-kommandos").classList.add("inactive")
+            document.querySelector("#terminal-kommandos").classList.remove("active")
+            document.querySelector("#terminal-kommandos").classList.add("inactive")
+            break;
+    }
+}
